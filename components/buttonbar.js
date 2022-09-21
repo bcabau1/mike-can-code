d986dc87878d6d3ca02616aaad09d7b944fdac01
Brian Cabau <43051275+bcabau1@users.noreply.github.com>
2022-09-21 01:29:09 -0400

    dropshadow add on indexpage

--- components/buttonbar.js
+++ components/buttonbar.js
@@ -10,7 +10,7 @@ export default function ButtonBar({children}) {
     return (
         <Box w='100%' mt='6' position='relative' >
             <NextLink href='#banners'>
-                <Button display='block' margin='auto' borderRadius='full' w='calc(35% + 75px)' bg={useColorModeValue('#8bd3dd', '#2cb67d')} fontFamily='heading'>
+                <Button display='block' margin='auto' borderRadius='full' w='calc(35% + 75px)' bg={useColorModeValue('#8bd3dd', '#2cb67d')} fontFamily='heading' boxShadow={useColorModeValue('lg', 'dark-lg')}>
                             <Text as='h3' fontSize='calc(8px + .5vw)' >{children}</Text>
                 </Button>
             </NextLink>
--- components/snippetbio.js
+++ components/snippetbio.js
@@ -7,7 +7,7 @@ export default function SnippetBio({children})    {
     return (
         <>
         <Box minH="10px" ml={12} my='6' w='100%' >
-                <Center><Image src='/images/profilepic.jpg' borderRadius='full' boxSize='calc(85px + 7.5vw)' mb='5' border='3px solid white'/></Center>
+                <Center><Image src='/images/profilepic.jpg' borderRadius='full' boxSize='calc(85px + 7.5vw)' mb='5' border='3px solid white' boxShadow={useColorModeValue('lg', 'dark-lg')}/></Center>
                 <Text noOfLines={8} textAlign='center' fontSize='calc(10px + .5vw)' fontFamily={'snippit'} color={useColorModeValue('#2d333a','#fffffe')}>
                     {children}
                 </Text>
