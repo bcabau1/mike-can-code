96b977ca884a44a7ff4378cc62e1c7013914c5d5
Brian Cabau <43051275+bcabau1@users.noreply.github.com>
2022-10-10 11:38:33 -0400

    experience page layout
    timeline component, work experience objects

--- components/timeline.js
+++ components/timeline.js
@@ -2,21 +2,59 @@ import { Box, Container } from "@chakra-ui/layout"
 import Card from "./card"
 
 
-export const TimelineElement = ({children}) =>  {
+const TimelineElement = ({props, children}) =>  {
 
     return (
         <Card>
-            <Box display='flex'>
-                { children }
+            <Box w='100%' h='100%'>
+                    {children}
             </Box> 
         </Card>       
     )
 }
 
+const experiences = [
+    {
+    id: 0, 
+    title: 'Software Developer', 
+    company: 'General Motors', 
+    startDate:'June, 2021', 
+    endDate:'May, 2022', 
+    description:'',
+    skills: ''
+    },
+    {
+    id: 1, 
+    title: 'Software Engineer Intern', 
+    company: 'Blue Yonder', 
+    startDate:'May, 2020', 
+    endDate:'August, 2020', 
+    description:'',
+    skills: ''
+    },
+    {
+    id: 2, 
+    title: 'Research Intern (Software Engineering)', 
+    company: 'Condensed Matter Laboratory (GSU)', 
+    startDate:'May, 2019', 
+    endDate:'December, 2020', 
+    description:'',
+    skills: ''
+    },
+    {
+    id: 3, 
+    title: 'Supplemental Instructor', 
+    company: 'Georgia State University', 
+    startDate:'January, 2019', 
+    endDate:'December, 2019', 
+    description:'',
+    skills: ''
+    }]
+
 export default function Timeline()  {
     return (
-        <Container>
-            <TimelineElement></TimelineElement>
-        </Container>
+        <Box minH='400px' w='100%' border='1px solid white'>
+            <TimelineElement/>
+        </Box>
     )
 }
--- pages/experience.js
+++ pages/experience.js
@@ -1,6 +1,7 @@
 import PageAnimate from "../components/layouts/pageanimate"
 import { Heading, Flex, Divider, Text, useColorModeValue } from "@chakra-ui/react"
 import AttributeFooter from "../components/footer"
+import Timeline from "../components/timeline"
 
 
 export default function Experience()  {
@@ -9,14 +10,14 @@ export default function Experience()  {
 
     return (
         <PageAnimate title='Experience'>
-            <Flex position='relative' w = '100%' h='100%' textAlign='center' flexWrap='wrap' overflow='auto' alignItems='center' justifyContent='center' py='6%'>
-                <Flex w='100%'  flexWrap='wrap' overflow='auto' alignItems='center' justifyContent='center' fontFamily='snippit'>
+            <Flex position='relative' w = '100%' h='100%' flexWrap='wrap' overflow='auto' alignItems='center' py='6%'>
+            <Flex w='100%'  flexWrap='wrap' overflow='auto' alignItems='center' justifyContent='center' fontFamily='snippit'>
                     <Heading w='100%' as="h3" variant="tab-title" id='proj' mb='6' textAlign={'left'}>
                     <Text textAlign='left' lineHeight='1.5em'>Experience</Text>
                     <Divider bg={useColorModeValue('#8bd3dd', '#2cb67d')} w={dividerLength} h='calc(3px + .25vw)' borderRadius='full' orientation='horizontal' />
                     </Heading>
-                </Flex>
-                
+                    <Timeline/>       
+                </Flex>         
             </Flex>
             <AttributeFooter>© 2022 Brian Cabau. All Rights Reserved. Icons created by IwitoStudio - Flaticon</AttributeFooter>
         </PageAnimate>  )
